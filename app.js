35ae68f7d8c6a1dbaa4d5050d74b2227f211521e
Hawei <hawei@paypal.com>
2020-05-06 15:16:54 +0800

    feat: SSR基本原理.

---
+++ app.js
@@ -0,0 +1,19 @@
+const reactSsr = require('./dist/src/server/middlewares/react-ssr').default;
+
+const Koa = require('koa2');
+const koaStatic =require('koa-static');
+const path = require('path');
+
+const app = new Koa();
+
+
+app.use(koaStatic(
+    path.join(__dirname, './dist/static')
+));
+
+app.use(reactSsr);
+
+app.listen(9001);
+
+console.log("run in 9001 port");
+
